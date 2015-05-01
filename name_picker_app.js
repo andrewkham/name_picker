edb87785986184dd441d61fa31c778490d26868e
Andrew Ham <hampossible@gmail.com>
2015-04-30 17:36:17 -0700

    completed random pick button

--- name_picker_app.js
+++ name_picker_app.js
@@ -1,8 +1,10 @@
 NBAteams = ["Celtics", "Nets", "Knicks", "76ers", "Raptors", "Bulls", "Cavaliers", "Pistons", "Pacers", "Bucks", "Hawks", "Hornets", "Heat", "Magic", "Wizards", "Warriors", "Clippers", "Lakers", "Suns", "Kings", "Mavericks", "Rockets", "Grizzlies", "Pelicans", "Spurs", "Nuggets", "Timberwolves", "Thunder", "Blazers", "Jazz"]
-function NBAteam (){
+function NBAteam(){
 	var pick = Math.floor(Math.random() * NBAteams.length)
 	var namePicker = NBAteams[pick];
 	console.log(namePicker);
+	return namePicker;
 	}
-
-
+function NBAbutton() {
+	document.getElementsByTagName("h1")[0].innerHTML = NBAteam();
+}
